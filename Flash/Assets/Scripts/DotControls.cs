1c5260153bd7bfc16484dfd54063796546140cc9
TramTran2496 <1414145@hcmut.edu.vn>
2018-04-28 12:17:36 +0700

    Turn left right on touching

--- Flash/Assets/Scripts/DotControls.cs
+++ Flash/Assets/Scripts/DotControls.cs
@@ -9,13 +9,17 @@ public class DotControls : MonoBehaviour {
 	public float speed = 1.0f;
 	public float yPos = -6.0f;
 
+	private float LRmost = 1.25f;
+	private int LRsteps = 5;
+
 	void Start () {
 		rigidbody = GetComponent<Rigidbody2D>();	
 		renderer = GetComponent<Renderer>();
 	}
 
 	void Update () {
-		if (transform.position.y >= -2){
+		if (rigidbody.gravityScale != 0 && transform.position.y >= -2){
+			rigidbody.gravityScale = 0;
 			rigidbody.Sleep ();
 			yPos = transform.position.y;
 		}
@@ -41,17 +45,49 @@ public class DotControls : MonoBehaviour {
 	}
 
 	void tappingHandle(){
+		// for mobile
 		if (Input.touchCount > 0){
-			if (Input.GetTouch (0).position.x < Screen.width / 2 && transform.position.x >= -1f)
-				transform.Translate (new Vector3 (-0.5f, 0.0f));
-			else if (Input.GetTouch (0).position.x > Screen.width / 2 && transform.position.x <= 1f)
-				transform.Translate (new Vector3 (0.5f, 0.0f));
+			if (Input.GetTouch (0).position.x < Screen.width / 2 && transform.position.x > -1f)
+				transform.Translate (new Vector3 (-LRmost / LRsteps, 0.0f));
+			else if (Input.GetTouch (0).position.x > Screen.width / 2 && transform.position.x < 1f)
+				transform.Translate (new Vector3 (LRmost / LRsteps, 0.0f));
 		}
 		else{
-			if (transform.position.x < 0)
-				transform.Translate (new Vector3 (0.5f, 0.0f));
-			else if (transform.position.x > 0)
-				transform.Translate (new Vector3 (-0.5f, 0.0f));
+			if (transform.position.x < 0){
+				if (transform.position.x < -LRmost / LRsteps)
+					transform.Translate (new Vector3 (LRmost / LRsteps, 0.0f));
+				else
+					transform.Translate (new Vector3 (-transform.position.x, 0.0f));
+			}
+			else if (transform.position.x > 0){
+				if (transform.position.x > LRmost / LRsteps)
+					transform.Translate (new Vector3 (-LRmost / LRsteps, 0.0f));
+				else
+					transform.Translate (new Vector3 (-transform.position.x, 0.0f));
+			}
 		}
+		/* for pc testing
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			if (transform.position.x < LRmost)
+				transform.Translate (new Vector3 (LRmost / LRsteps, 0.0f));
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow)) {
+			if(transform.position.x > -LRmost)
+				transform.Translate (new Vector3 (-LRmost / LRsteps, 0.0f));
+		}
+		else {
+			if (transform.position.x < 0){
+				if (transform.position.x < -LRmost / LRsteps)
+					transform.Translate (new Vector3 (LRmost / LRsteps, 0.0f));
+				else
+					transform.Translate (new Vector3 (-transform.position.x, 0.0f));
+			}
+			else if (transform.position.x > 0){
+				if (transform.position.x > LRmost / LRsteps)
+					transform.Translate (new Vector3 (-LRmost / LRsteps, 0.0f));
+				else
+					transform.Translate (new Vector3 (-transform.position.x, 0.0f));
+			}
+		}*/
 	}
 }
