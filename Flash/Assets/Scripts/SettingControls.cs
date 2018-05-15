cf0040b81922bde82e7ebd0c9d6e39209bf3a782
BNghi <36978879+BNghi@users.noreply.github.com>
2018-05-16 02:23:21 +0700

    handle music settings

--- Flash/Assets/Scripts/SettingControls.cs
+++ Flash/Assets/Scripts/SettingControls.cs
@@ -6,9 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class SettingControls : MonoBehaviour {
 	private Image buttonImage;
-
 	private Sprite muteSprite;
 	private Sprite unmuteSprite;
+	private MusicClass music;
+	private Slider slider;
+
 	private bool mute = false;
 
 	// Use this for initialization
@@ -16,6 +18,8 @@ public class SettingControls : MonoBehaviour {
 		buttonImage = GameObject.Find ("MusicButton").GetComponent<Image>();
 		muteSprite = Resources.Load<Sprite> ("style01/shape010_style01_color08");
 		unmuteSprite = Resources.Load<Sprite>("style01/shape009_style01_color08");
+		music = GameObject.Find ("SoundManager").GetComponent<MusicClass> ();
+		slider = GameObject.Find ("Slider").GetComponent<Slider> ();
 	}
 	
 	// Update is called once per frame
@@ -27,13 +31,19 @@ public class SettingControls : MonoBehaviour {
 		//TODO: handle music setting
 		if (mute) {
 			buttonImage.sprite = unmuteSprite;
+			music.PlayMusic ();
 		} else {
 			buttonImage.sprite = muteSprite;
+			music.StopMusic ();
 		}	
 		mute = !mute;
 	}
 
 	public void backButtonAction() {
 		SceneManager.LoadScene (0);
+	}
+
+	public void setMusicVolume() {
+		music._audioSource.volume = slider.value;
 	}
 }
